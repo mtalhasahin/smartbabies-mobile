6891886df9009d935bd9df7e87b2cc399a6f74ea
Batuhan ÇIKRIKCI <batuhancikrikci@gmail.com>
2014-02-21 20:03:57 +0200

    created hybrid application with ibm worklight

---
+++ apps/smartbabies/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":1181583930,"date":1393005152636,"machine":"opensuse.vaio"};
+/* Date: Fri Feb 21 19:52:32 EET 2014 */
---
+++ apps/smartbabies/android/native/src/com/smartbabies/GCMIntentService.java
@@ -0,0 +1,5 @@
+package com.smartbabies;
+
+public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentService{
+	//Nothing to do here...
+}
